a7ff3856180a144b8aca229d28cc4e6095d04465
Sanjana-n-g <sanjananjg20@gmail.com>
2026-02-18 15:32:38 +0530

    Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,6 +1,6 @@
 public class helloworld {
-  public static void main(string[] args) {
-    System.out.println("Hello World");
+  public static void main(String[] args) {
+    System.out.println("HelloWorld");
   }
 }  
   
